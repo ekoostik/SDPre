23998dde981965213550635667ba8315953e8005
David Dunlevy <david.dunlevy@gmail.com>
2022-12-21 11:17:56 +1300

    Completed Obj1Lab1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,7 +1,7 @@
 public class Objective1Lab1 {
   public static void main(String[] args) {
-    /*
-      Your solution goes here
-    */
+    
+    System.out.println("Hello, Skill Distillery") ;
+    
   }
 }
