81b2bf7cdccac4af2ad21eb6e025af2085dcf0c5
David Dunlevy <david.dunlevy@gmail.com>
2022-12-21 12:37:31 +1300

    Completed Obj2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Name: David Dunlevy Age:";
+    int age = 39;
+    String state = "State: Ohio";
+    
+
+    System.out.println(name + " " +age + " " +state);
+
+    
+  }
+}
