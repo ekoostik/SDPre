05afe029ad60fd3446d3b5336f32c120245f7d7e
David Dunlevy <david.dunlevy@gmail.com>
2022-12-21 13:56:59 +1300

    Completed Obj3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,14 @@
+public class Objective3Lab2 {
+	public static void main(String[] args){
+
+		double side1 = 10;
+		double side2 = 8;
+		double hypotenuse = side1*side1 + side2*side2;
+		double result = Math.sqrt(hypotenuse);
+
+		System.out.println(result);
+		
+
+
+	}
+}
