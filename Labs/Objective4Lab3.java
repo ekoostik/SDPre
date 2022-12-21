6365a9a198bd82733397384ed14c26bd619b9d64
David Dunlevy <david.dunlevy@gmail.com>
2022-12-21 16:58:16 +1300

    Completed Obj4Lab3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Objective4Lab3{
+	public static void main(String[] args){
+
+	
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("How old are you?");
+		int age = input.nextInt();
+
+		int birthYear = 2022 - age;
+
+		System.out.println("You were born in " + birthYear);
+		input.close();
+
+
+
+	}
+}
