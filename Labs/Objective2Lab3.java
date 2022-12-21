3fdf2de1480fac96df410f0ce59b86c6438c528d
David Dunlevy <david.dunlevy@gmail.com>
2022-12-21 13:03:42 +1300

    Completed Obj2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,20 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1983;
+    String month = "March";
+    boolean isWeekend = true; 
+    boolean isntWeekend = false;
+
+    
+
+    
+
+    System.out.println(year);
+    System.out.println(month);
+
+
+
+
+  }
+}
