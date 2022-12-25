8d50959493d9c228b31e75c9d05db99f45ce295b
David Dunlevy <david.dunlevy@gmail.com>
2022-12-25 19:26:57 +1300

    Completed Obj9Lab5

--- Labs/Objective9Lab5.java
+++ Labs/Objective9Lab5.java
@@ -6,7 +6,6 @@ public class Objective9Lab5 {
     Scanner kb = new Scanner (System.in);
 		double num1, num2;
 		boolean keepGoing = true;
-		int choice;
 		double answer = 0.0;
 
     // Part 1:
@@ -40,27 +39,27 @@ public class Objective9Lab5 {
 			// Don't forget to account for a user accidentally inputting an invalid number!
       
       case 1:
-      		System.out.println(num1 + "+ " + num2 + "= " + sum);
+      		System.out.println(num1 + " + " + num2 + " = " + sum);
       		break;
       case 2:
-      		System.out.println("The average of " + num1 + "and " + num2 + "is: " + average);
+      		System.out.println("The average of " + num1 + " and " + num2 + " is: " + average);
       		break;
       case 3:
-      		System.out.println("The amount in tax to be collected from a purchase of " + num1 + "and " + num2 + "is: " + tax);
+      		System.out.println("The amount in tax to be collected from a purchase of " + num1 + " and " + num2 + " is: " + tax);
       		break;
       case 4:
-      	System.out.println("You've chosen to quit.");
-      	break;
+      		System.out.println("You've chosen to quit.");
+      		break;
       default: 
-      	System.out.println("Invalid entry. Please try again");
-      		
-
-		}
+      		System.out.println("Invalid entry. Please try again");
+      			
+      		}
+      	}
 		kb.close();
 
+		
 	}
-
- public static void printMenu() {
+	public static void printMenu(){
 		System.out.println();
 		System.out.println("========= MENU =========");
 		System.out.println("|                      |");
@@ -71,30 +70,18 @@ public class Objective9Lab5 {
 		System.out.println("|                      |");
 		System.out.println("========================");
 		System.out.println();
-	}
-
- public static double findSum(double x, double y) {
+	  }
+	  public static double findSum(double x, double y){
     double sum = x + y;
     return sum;
-	}
-  // Part 4:
-  // Write a method named "findAverage" that takes two doubles as parameters,
-  // and returns a double that represents the average of the two parameters.
-  // Use the "findSum" method as an example.
- public static double findAverage(double a, double b){
+	  }
+	  public static double findAverage(double a, double b){
  		double average = (a + b) / 2;
  		return average;
- }
-
-	// Part 5:
-  // Write a method named "calcTax" that also takes two doubles as parameters,
-  // and returns a double that represents the amount of tax the user would have
-  // to pay based on the total of the two parameters. Use a tax rate of 8.31%
-  // (Not a grand total, just the total taxes.  Refer to "Expected Output")
- 
- public static double calcTax(double n, double m){
- 		double tax = (n + m) * 8.31;
+    }
+    public static double calcTax(double n, double m){
+ 		double tax = (n + m) * .0831;
  		return tax;
- }
+    }
 
 }
